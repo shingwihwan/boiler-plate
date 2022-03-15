bb63d455f6e5f6d25a88d0b0638fd123143705f4
zamar <zerll9100@gmail.com>
2022-03-15 23:56:55 +0900

    14강 로그아웃

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
 
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User')
 
 // application/x-www-form-urlencoded
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
@@ -36,7 +37,7 @@ app.post('/register', (req, res) => {
     });
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -63,10 +64,34 @@ app.post('/login', (req, res) => {
             })
         }
     })
-
-
 });
 
+// role 1 어드민 role 2 특정 부서 어드민
+// role 0 -> 일반유저 role 0이 아니면 관리자
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과했다는 얘기는 Authentication 이 True 라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" },
+        (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
